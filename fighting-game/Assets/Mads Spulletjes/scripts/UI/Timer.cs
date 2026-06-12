4680f77656e6741ad8578f6a87bedeb5d0b4894d
SampiePlays <38857@ma-web.nl>
2026-06-12 12:00:57 +0200

    pacoscripts

---
+++ fighting-game/Assets/Mads Spulletjes/scripts/UI/Timer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] private Text timerText;
+
+    private float timeRemaining = 90f;
+    private bool timerRunning = true;
+
+    private void Start()
+    {
+        timerText = GetComponent<Text>();
+        timerText.text = "90";
+    }
+
+    private void Update()
+    {
+        if (timerRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+            timerText.text = Mathf.Round(timeRemaining).ToString();
+
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                timerRunning = false;
+                timerText.text = "End";
+            }
+        }
+    }
+}
+
